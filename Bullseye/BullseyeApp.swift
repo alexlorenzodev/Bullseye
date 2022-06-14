1fd9727165eac12c71cc9944428fd3ea51930f34
alex <AlexanderLorenzo@miamibeachfl.gov>
2022-06-14 16:02:26 -0400

    Initial Commit

---
+++ Bullseye/BullseyeApp.swift
@@ -0,0 +1,17 @@
+//
+//  BullseyeApp.swift
+//  Bullseye
+//
+//  Created by alex on 6/14/22.
+//
+
+import SwiftUI
+
+@main
+struct BullseyeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
