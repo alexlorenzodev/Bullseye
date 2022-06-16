b438c392daaa45861fe17a177f536634b4820ec1
AlexanderLorenzo <alexlorenzodev@gmail.com>
2022-06-15 22:04:52 -0400

    added some modifiers to text view

--- Bullseye/ContentView.swift
+++ Bullseye/ContentView.swift
@@ -10,7 +10,12 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Text("PUT THE BULLSEYE AS CLOSE AS YOU CAN TO!")
+            Text("PUT THE BULLSEYE AS CLOSE AS YOU CAN TO").bold()
+                .kerning(/*@START_MENU_TOKEN@*/1.0/*@END_MENU_TOKEN@*/)
+                .multilineTextAlignment(.center)
+                .lineSpacing(4)
+                .font(.footnote)
+                
             Text("89")
             HStack {
                 Text("1")
@@ -28,6 +33,8 @@ struct ContentView: View {
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+            .environment(\.sizeCategory, .extraLarge)
+            .previewInterfaceOrientation(.portrait)
         ContentView()
             .previewLayout(.fixed(width: 568, height: 320))
     }
